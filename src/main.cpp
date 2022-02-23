ae04dd4e2d8383f1229843efb298f0cfc9695a62
Tomáš Borek <tomas.borek@favorlogic.com>
2022-02-23 16:24:42 +0100

    Add LED flashing and serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define GROWBOX_VERSION "0.1"
+
+/**
+ * Flashes built-in LED.
+ * 
+ * @param count Number of flashes.
+ * @param up Time when LED lights.
+ * @param down Time when LED does not light. 
+ */ 
+void flash(int count = 3, int up = 20, int down = 50) {
+    for (int i = 0; i < count; i++) {
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(up);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(down);
+    }
 }
 
+/**
+ * Setup application.
+ */
+void setup() {
+    Serial.begin(9600);
+    Serial.print(F("GrowBox "));
+    Serial.println(F(GROWBOX_VERSION));
+
+    // Use built-in LED to signalize status
+    pinMode(LED_BUILTIN, OUTPUT);
+    flash(2, 200, 100);
+
+    Serial.println(F("GrowBox initialized"));
+}
+
+/**
+ * Main application loop.
+ */
 void loop() {
-  // put your main code here, to run repeatedly:
+    flash();
+    delay(1000);
 }
